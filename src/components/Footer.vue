3e70d7d55bcc765736dbb51986594513b7ba1f83
dandnirv7 <dandnirv7@gmail.com>
2023-09-16 10:00:52 +0700

    refactor: add component (Footer & ScrollUpButton)

---
+++ src/components/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="flex flex-col space-y-2 items-center justify-center px-5 bg-gray-200 py-5 md:flex-row md:justify-between"
+  >
+    <div
+      class="w-full md:w-max flex flex-col space-y-2 items-center justify-start md:flex-row md:space-x-4"
+    >
+      <h2 class="text-sm font-semibold md:text-base">Socials</h2>
+      <div class="flex flex-row space-x-2">
+        <a href="https://www.linkedin.com/in/dandi-nirpana-nahin-729a34214/">
+          <img
+            src="../assets/images/icons/socials/linkedin.svg"
+            alt=""
+            class="w-8 md:w-10"
+          />
+        </a>
+        <a href="https://twitter.com/dandnirv7">
+          <img
+            src="../assets/images/icons/socials/x.svg"
+            alt=""
+            class="w-8 md:w-10"
+          />
+        </a>
+        <a href="https://instagram.com/dandnirv">
+          <img
+            src="../assets/images/icons/socials/instagram.svg"
+            alt=""
+            class="w-8 md:w-10"
+          />
+        </a>
+        <a href="mailto:dandinirpana7@gmail.com">
+          <img
+            src="../assets/images/icons/socials/gmail.svg"
+            alt=""
+            class="w-8 md:w-10"
+          />
+        </a>
+      </div>
+    </div>
+    <span class="text-center">&copy; 2023 Dandnirv</span>
+  </div>
+</template>
---
+++ src/components/ScrollUpButton.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const showButton = ref(false);
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+const handleScroll = () => {
+  if (window.scrollY > 200) {
+    showButton.value = true;
+  } else {
+    showButton.value = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
+
+<template>
+  <button
+    @click="scrollToTop"
+    v-show="showButton"
+    class="fixed bottom-4 right-4 bg-amber-500 text-white px-4 py-2 w-10 h-10 lg:w-12 lg:h-12 md:w-10 md:h-10 rounded-full shadow-md cursor-pointer flex items-center justify-center"
+  >
+    <i class="material-symbols-outlined">arrow_drop_up</i>
+  </button>
+</template>
+
+<style scoped>
+.material-symbols-outlined {
+  font-size: 28px;
+  font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 0;
+}
+
+@media screen and (min-width: 768px) and (max-width: 1023px) {
+  .material-symbols-outlined {
+    font-size: 32px;
+    font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 0;
+  }
+}
+@media screen and (min-width: 1024px) {
+  .material-symbols-outlined {
+    font-size: 40px;
+    font-variation-settings: "FILL" 1, "wght" 400, "GRAD" 0, "opsz" 0;
+  }
+}
+</style>
